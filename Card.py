6f4d7fb1c964081e8c32f9f8085cd04bd4f16e9b
Chris Sulz <chris.sulz@xtelinternational.com>
2013-04-16 17:08:20 -0600

    Added essential functions and tests of those functions to Card and Deck

--- Card.py
+++ Card.py
@@ -34,14 +34,23 @@ class Card():
     
     def __str__(self):
         return self.getRank()+" of " + self.getSuit()
+    
+    def __cmp__(self,other):
+        if self.rank != other.rank:
+            return self.rank-other.rank
+        else:
+            return self.suit-other.suit
+            
+    def compareRanks(self,other):
+        return self.rank-other.rank
         
         
 def _test():
     myCard = Card(1,0)
     print myCard
     
-    for s in range(4):
-        for r in range(1,14): 
+    for s in xrange(4):
+        for r in xrange(1,14): 
             myCard = Card(r,s)
             print myCard
             
@@ -49,26 +58,165 @@ def _test():
         Card(0,1)
     except ValueError as error:
         print error
+    except:
+        assert False,"Raised Wrong exception type"
+    else:
+        assert False,"Creation of invalid card did not raise exception"
         
     try:
         Card(14,1)
     except ValueError as error:
         print error
+    except:
+        assert False,"Raised Wrong exception type"
+    else:
+        assert False,"Creation of invalid card did not raise exception"
         
     try:
         Card(1,6)
     except ValueError as error:
         print error
+    except:
+        assert False,"Raised Wrong exception type"
+    else:
+        assert False,"Creation of invalid card did not raise exception"
         
     try:
         Card(1,-1)
     except ValueError as error:
         print error
+    except:
+        assert False,"Raised Wrong exception type"
+    else:
+        assert False,"Creation of invalid card did not raise exception"
         
     try:
         Card(0,-1)
     except ValueError as error:
         print error
+    except:
+        assert False,"Raised Wrong exception type"
+    else:
+        assert False,"Creation of invalid card did not raise exception"
+        
+    myCard = Card(1,0)
+    myOther = Card(1,0)
+   
+    print myCard==myOther
+    assert myCard==myOther,"Cards not equal"
+    
+    print not myCard<myOther
+    assert not myCard<myOther,"Card greater than or equal to other"
+    
+    print myCard<=myOther
+    assert myCard<=myOther,"Card is greater than other"
+    
+    print not myCard>myOther
+    assert not myCard>myOther,"Card is greater than other"
+    
+    print myCard>=myOther
+    assert myCard>=myOther,"Card is less than other"
+    
+    myOther = Card(1,2)#change suit, does not change rank
+    print not myCard==myOther
+    assert not myCard==myOther,"Cards equal"
+    
+    print myCard!=myOther
+    assert myCard!=myOther,"Cards equal"
+    
+    print myCard<myOther
+    assert myCard<myOther,"Card not greater than other"
+    
+    print myCard<=myOther
+    assert myCard<=myOther,"Card is greater than other"
+    
+    print not myCard>myOther
+    assert not myCard>myOther,"Card is greater than other"
+    
+    print not myCard>=myOther
+    assert not myCard>=myOther,"Card is less than equal to other"
+    
+    myOther = Card(2,2)#change suit and rank
+    print not myCard==myOther
+    assert not myCard==myOther,"Cards equal"
+    
+    print myCard!=myOther
+    assert myCard!=myOther,"Cards equal"
+    
+    print myCard<myOther
+    assert myCard<myOther,"Card not greater than other"
+    
+    print myCard<=myOther
+    assert myCard<=myOther,"Card is greater than other"
+    
+    print not myCard>myOther
+    assert not myCard>myOther,"Card is greater than other"
+    
+    print not myCard>=myOther
+    assert not myCard>=myOther,"Card is less than equal to other"
+    
+    myCard,myOther = (myOther,myCard,)#swap cards
+    
+    print not myCard==myOther
+    assert not myCard==myOther,"Cards equal"
+    
+    print myCard!=myOther
+    assert myCard!=myOther,"Cards equal"
+    
+    print not myCard<myOther
+    assert not myCard<myOther,"Card not greater than other"
+    
+    print not myCard<=myOther
+    assert not myCard<=myOther,"Card is greater than other"
+    
+    print myCard>myOther
+    assert myCard>myOther,"Card is greater than other"
+    
+    print myCard>=myOther
+    assert myCard>=myOther,"Card is less than equal to other"
+    
+    cards = [Card(i,0) for i in xrange(1,14)]
+    from random import shuffle
+    
+    copy = list(cards)
+    assert cards==copy
+    
+    while cards==copy:
+        shuffle(cards)
+    
+    assert cards!=copy
+    
+    sort = sorted(cards)
+    
+    for card in cards:
+        print card
+    
+    for card in sort:
+        print card
+        
+    assert cards!=sort
+    
+    cards = [Card(1,i) for i in xrange(0,4)]
+    
+    copy = list(cards)
+    assert cards==copy
+    
+    while cards==copy:
+        shuffle(cards)
+    
+    assert cards!=copy
+    
+    sort = sorted(cards)
+    
+    for card in cards:
+        print card
+    
+    for card in sort:
+        print card
+        
+    assert cards!=sort
+    
+
 
 if __name__ == '__main__':
     _test()
--- Deck.py
+++ Deck.py
@@ -2,80 +2,112 @@ import Card
 
 class Deck():
     def __init__(self, empty=False, cards=[]):
-        self.deck = cards
-
+        self.deck = list(cards)
+        
         if len(cards)==0 and not empty:
-            for suit in range(4):
-                for rank in range(1,14):
-                    self.deck.append(Card.Card(rank,suit))
-                
+            self.deck = [Card.Card(r,s) for s in xrange(4) for r in xrange(1,14)]
+            
     def __str__(self):
-        ret = ''
+        if len(self.deck)==0:
+            return '[]'
+        ret = '['
         for card in self.deck[::-1]:
             ret += str(card)+", "
             
-        ret = ret[:-2]
+        ret = ret[:-2]+']'
         return ret
         
-    # def __copy__(self):
-        # return Deck(self.deck[:])
+    def copy(self):
+        return Deck(cards=self.deck[:])
         
     def __len__(self):
         return len(self.deck)
         
-    # def shuffle(self):
-        # from random import shuffle as sh
-        # sh(self.deck)
+    def __cmp__(self,other):
+        return cmp(self.deck,other.deck)
         
-    # def drawCard(self):
-        # try:
-            # card = self.deck.pop()
-            # return card
-        # except IndexError:
-            # raise IndexError('No more cards in deck')
+    def drawCard(self):
+        try:
+            return self.deck.pop()
+        except IndexError:
+            raise IndexError('No more cards in deck')
+        
+    def pop(self):
+        return self.drawCard()
             
-    # def insertCard(self,card):
-        # if isinstance(card, Card.Card):
-            # self.deck.append(card)
-        # else:
-            # raise ValueError('Card not instance of Card()')
+    def append(self,card):
+        if isinstance(card, Card.Card):
+            self.deck.append(card)
+        else:
+            raise ValueError('Card not instance of Card()')
+            
+    def shuffle(self):
+        from random import shuffle as sh
+        sh(self.deck)
         
-    # def __add__(self,card):
-        # self.insertCard(card)
+    def __add__(one, two):
+        if isinstance(one,Deck) and isinstance(two,Deck):
+            return Deck(cards=one.deck[:] + two.deck[:])
+        else:
+            raise TypeError(' can only concatenate Deck not "'+type(other)+'"to Deck')
+        
     
 def _test():
     myDeck = Deck()
 
-    # print myDeck
-    # print len(myDeck)
-    # assert len(myDeck)==52
+    print myDeck
+    print len(myDeck)==52
+    assert len(myDeck)==52
     
     myDiscard = Deck(empty=True)
     
     print myDiscard
-    print len(myDiscard)
+    print len(myDiscard)==0
     assert len(myDiscard)==0
     
-    # myCard = myDeck.drawCard()
-    # print myCard
-    # print myDeck
-    # print len(myDeck)
-    # assert len(myDeck)==51
+    myCard = myDeck.drawCard()
+    print myCard
+    print myDeck
+    print len(myDeck)==51
+    assert len(myDeck)==51
     
-    # myDiscard.insertCard(myCard)
+    myDiscard.append(myCard)
+    print myDiscard
+    print len(myDiscard)==1
+    assert len(myDiscard)==1
     
-    # print myDiscard
-    # print len(myDiscard)
-    # assert len(myDiscard)==1
+    myCard = myDeck.pop()
+    print myCard
+    print myDeck
+    print len(myDeck)==50
+    assert len(myDeck)==50
     
-    # myDeck.shuffle()
+    myDiscard.append(myCard)
+    print myDiscard
+    print len(myDiscard)==2
+    assert len(myDiscard)==2
     
-    # print myDeck
-    # myCard = myDeck.drawCard()
-    # print myCard
-    # print myDeck
-    # print len(myDeck)
-    # assert len(myDeck)==50
+    myCopyDeck = myDiscard.copy()
+    
+    print myCopyDeck
+    print len(myCopyDeck)==2
+    assert len(myCopyDeck)==2
+    print myDiscard==myCopyDeck
+    
+    myCopyDeck = myDeck.copy()
+    myDeck.shuffle()
+    
+    print myDeck
+    print myDeck!=myCopyDeck
+    assert myDeck!=myCopyDeck
+    
+    myDeck = myDeck+myDiscard
+    print myDeck
+    print len(myDeck)==52
+    assert len(myDeck)==52
+    
+    print myDeck!=myCopyDeck
+    assert myDeck!=myCopyDeck
     
 
 if __name__ == '__main__':
